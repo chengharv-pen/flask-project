82f6dec7609bad13509c0f872ff34fbdde3cd548
cheng <chengharvp@gmail.com>
2024-08-26 15:48:12 -0400

    comment about secret key

--- market/__init__.py
+++ market/__init__.py
@@ -5,7 +5,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+
+# Please hide the secret key using a better way in prod.
 app.config['SECRET_KEY'] = '759d75c19590912602a548e0'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
